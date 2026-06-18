032c6bf7c5bc2f9f89ac35f313bd00983cca67d9
pak-mat <pak-mat@users.noreply.github.com>
2026-06-18 16:21:10 +0800

    Revert base path for friend's repo

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/saina-care/',
+    base: '/SAINA-Care-Portal/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
